8d0bd9e95f5659ae6d9a9049ec1aa247f08291f1
Tech-A12 <tech-a12@Tech-A12s-Air.home>
2018-02-18 14:06:19 +0000

    Added basic user views, and basic links in users_controller

---
+++ controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < Sinatra::Base
+
+  # sets root as the parent-directory of the current file
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  # sets the view directory correctly
+  set :views, Proc.new { File.join(root, "views") }
+
+  configure :development do
+      register Sinatra::Reloader
+  end
+
+  # index
+  get '/users' do
+    "Index"
+  end
+
+  # new
+  get '/users/new' do
+    "New"
+  end
+
+  # create
+  post '/users' do
+
+  end
+
+  # show
+  get '/users/:id' do
+    id = params[:id].to_i
+    "Show"
+  end
+
+  # delete
+  delete '/users/:id' do
+
+  end
+
+  # edit
+  get '/users/:id/edit' do
+    id = params[:id].to_i
+    "Edit"
+  end
+
+  # update
+  put '/users/:id' do
+
+  end
+end
